b5a56a44c03d871bcbadb89991f4b5e7031a30bc
yj0216 <yeojun0216@naver.com>
2023-10-10 20:44:58 +0900

    프로젝트 초기 설정

---
+++ src/mocks/index.tsx
@@ -0,0 +1,9 @@
+// const initMockAPI = async (): Promise<void> => {
+//     if (typeof window === 'undefined') {
+//       const { server } = await import('../mocks/server');
+//       server.listen();
+//     } else {
+//       const { worker } = await import('../mocks/browser');
+//       worker.start({ onUnhandledRequest: 'bypass' });
+//     }
+//   };
